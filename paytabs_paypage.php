8edb263cffb044247216e1b5d93cbb8859c24ff5
wajih <wajih.km@gmail.com>
2020-07-06 09:35:13 +0400

    set the Version

--- paytabs_paypage.php
+++ paytabs_paypage.php
@@ -35,7 +35,7 @@ class PayTabs_PayPage extends PaymentModule
     {
         $this->name                   = 'paytabs_paypage';
         $this->tab                    = 'payments_gateways';
-        $this->version                = '2.0.3';
+        $this->version                = '2.1.0';
         $this->author                 = 'PayTabs';
         $this->controllers            = array('payment', 'validation');
         $this->currencies             = true;
